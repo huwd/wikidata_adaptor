7a24bc9195448f34ccbbcabf717a09afac204ea1
Huw Diprose <mail@huwdiprose.co.uk>
2025-11-12 23:46:06 +0000

    Refactor WikidataAdaptor::RestApi Statements into own module

--- lib/wikidata_adaptor/rest_api.rb
+++ lib/wikidata_adaptor/rest_api.rb
@@ -12,41 +12,14 @@ module WikidataAdaptor
     require_relative "rest_api/labels"
     require_relative "rest_api/descriptions"
     require_relative "rest_api/aliases"
+    require_relative "rest_api/statements"
     include WikidataAdaptor::RestApi::Items
     include WikidataAdaptor::RestApi::Sitelinks
     include WikidataAdaptor::RestApi::Properties
     include WikidataAdaptor::RestApi::Labels
     include WikidataAdaptor::RestApi::Descriptions
     include WikidataAdaptor::RestApi::Aliases
-
-    # Retrieve Statements from an Item.
-    #
-    # @param [String] The ID of the required Item.
-    # @param [String] The ID of the required Statement.
-    #
-    # @return [Hash] The statements of an Item.
-    def get_item_statements(item_id)
-      get_json("#{endpoint}/entities/items/#{CGI.escape(item_id)}/statements")
-    end
-
-    # Retrieve a single Statement from an Item.
-    #
-    # @param [String] The ID of the required Item.
-    # @param [String] The ID of the required Statement.
-    #
-    # @return [Hash] A single Wikibase Statement.
-    def get_item_statement(item_id, statement_id)
-      get_json("#{endpoint}/entities/items/#{CGI.escape(item_id)}/statements/#{statement_id}")
-    end
-
-    # Retrieve a single Statement from an Item.
-    #
-    # @param [String] The ID of the required Statement.
-    #
-    # @return [Hash] A single Wikibase Statement.
-    def get_statement(statement_id)
-      get_json("#{endpoint}/statements/#{statement_id}")
-    end
+    include WikidataAdaptor::RestApi::Statements
 
     # Retrieve an Item's sitelinks.
     #
@@ -65,25 +38,6 @@ module WikidataAdaptor
     # @return [Hash] Item's sitelink for the given site.
     def get_item_sitelink(item_id, site_id)
       get_json("#{endpoint}/entities/items/#{CGI.escape(item_id)}/sitelinks/#{CGI.escape(site_id)}")
-    end
-
-    # Retrieve Statements from a Property.
-    #
-    # @param [String] The ID of the required Property.
-    #
-    # @return [Hash] The statements of a Property.
-    def get_property_statements(property_id)
-      get_json("#{endpoint}/entities/properties/#{CGI.escape(property_id)}/statements")
-    end
-
-    # Retrieve a single Statement from a Property.
-    #
-    # @param [String] The ID of the required Property.
-    # @param [String] The ID of the required Statement.
-    #
-    # @return [Hash] A single Wikibase Statement.
-    def get_property_statement(property_id, statement_id)
-      get_json("#{endpoint}/entities/properties/#{CGI.escape(property_id)}/statements/#{statement_id}")
     end
   end
 end
--- lib/wikidata_adaptor/test_helpers/rest_api.rb
+++ lib/wikidata_adaptor/test_helpers/rest_api.rb
@@ -12,12 +12,14 @@ module WikidataAdaptor
       require_relative "rest_api/properties"
       require_relative "rest_api/labels"
       require_relative "rest_api/descriptions"
+      require_relative "rest_api/statements"
       include WikidataAdaptor::TestHelpers::RestApi::Aliases
       include WikidataAdaptor::TestHelpers::RestApi::Items
       include WikidataAdaptor::TestHelpers::RestApi::Sitelinks
       include WikidataAdaptor::TestHelpers::RestApi::Properties
       include WikidataAdaptor::TestHelpers::RestApi::Labels
       include WikidataAdaptor::TestHelpers::RestApi::Descriptions
+      include WikidataAdaptor::TestHelpers::RestApi::Statements
 
       WIKIBASE_REST_ENDPOINT = ENV["WIKIBASE_REST_ENDPOINT"] || "https://test.test/w/rest.php/wikibase/v0"
 
@@ -30,149 +32,6 @@ module WikidataAdaptor
         else
           stub_request(method, "#{WIKIBASE_REST_ENDPOINT}#{path}").with(**with).to_return(**to_return)
         end
-      end
-
-      #########################################
-      # GET /entities/items/:item_id/statements
-      #########################################
-      def stub_get_item_statements(item_id, response_body = nil)
-        stub_rest_api_request(
-          :get,
-          "/entities/items/#{item_id}/statements",
-          response_body: response_body || {
-            additionalProp1: [
-              {
-                id: "Q11$6403c562-401a-2b26-85cc-8327801145e1",
-                rank: "normal",
-                property: {
-                  id: "P92",
-                  "data-type": "string"
-                },
-                value: {
-                  content: "I am a goat",
-                  type: "value"
-                },
-                qualifiers: [],
-                references: []
-              }
-            ],
-            additionalProp2: [
-              {
-                id: "Q11$6403c562-401a-2b26-85cc-8327801145e1",
-                rank: "normal",
-                property: {
-                  id: "P92",
-                  "data-type": "string"
-                },
-                value: {
-                  content: "I am a goat",
-                  type: "value"
-                },
-                qualifiers: [],
-                references: []
-              }
-            ],
-            additionalProp3: [
-              {
-                id: "Q11$6403c562-401a-2b26-85cc-8327801145e1",
-                rank: "normal",
-                property: {
-                  id: "P92",
-                  "data-type": "string"
-                },
-                value: {
-                  content: "I am a goat",
-                  type: "value"
-                },
-                qualifiers: [],
-                references: []
-              }
-            ]
-          }
-        )
-      end
-
-      ########################################################
-      # GET /entities/items/:item_id/statements/:statement_id
-      ########################################################
-      def stub_get_item_statement(item_id, statement_id)
-        stub_rest_api_request(
-          :get,
-          "/entities/items/#{item_id}/statements/#{statement_id}",
-          response_body: {
-            id: statement_id.to_s,
-            rank: "normal",
-            property: {
-              id: "P92",
-              "data-type": "string"
-            },
-            value: {
-              content: "I am a goat",
-              type: "value"
-            },
-            qualifiers: [],
-            references: []
-          }
-        )
-      end
-
-      ###############################
-      # GET /statements/:statement_id
-      ###############################
-      def stub_get_statement(statement_id)
-        stub_rest_api_request(
-          :get,
-          "/statements/#{statement_id}",
-          response_body: {
-            id: statement_id.to_s,
-            rank: "normal",
-            property: {
-              id: "P92",
-              "data-type": "string"
-            },
-            value: {
-              content: "I am a goat",
-              type: "value"
-            },
-            qualifiers: [],
-            references: []
-          }
-        )
-      end
-
-      ############################################
-      # GET /entities/properties/:property_id/statements
-      ############################################
-      def stub_get_property_statements(property_id, response_body = nil)
-        stub_rest_api_request(
-          :get,
-          "/entities/properties/#{property_id}/statements",
-          response_body: response_body || {}
-        )
-      end
-
-      ##################################################################
-      # GET /entities/properties/:property_id/statements/:statement_id
-      ##################################################################
-      def stub_get_property_statement(property_id, statement_id)
-        stub_rest_api_request(
-          :get,
-          "/entities/properties/#{property_id}/statements/#{statement_id}",
-          response_body: {
-            id: statement_id.to_s,
-            rank: "normal",
-            property: {
-              id: property_id.to_s,
-              "data-type": "wikibase-item"
-            },
-            value: {
-              content: "Q5",
-              type: "value"
-            },
-            qualifiers: [],
-            references: []
-          }
-        )
       end
 
       private
